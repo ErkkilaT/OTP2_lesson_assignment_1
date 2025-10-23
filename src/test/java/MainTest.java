0190cd6e00f25aabd188b6ec53c54903ad457458
ErkkilaT <teemu.erkkila@gmail.com>
2025-10-23 14:10:33 +0300

    add testing for calculation

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,11 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +19,28 @@ class MainTest {
     void testFi(){
         ResourceBundle rb = ResourceBundle.getBundle("MessagesBundle", new Locale("fi","FI"));
         assertEquals("Syötä ostettavien tuotteiden määrä:", rb.getString("amountOfItems"));
+    }
+
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void mainCalcTest(){
+        String testInput = "1\n2\n10\n1\n5\n2\n";
+        System.setIn(new ByteArrayInputStream(testInput.getBytes()));
+        Main.main(new String[0]);
+        String testOutput = outContent.toString();
+        assertTrue(testOutput.contains("20"));
     }
 
 }
